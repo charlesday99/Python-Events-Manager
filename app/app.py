1e024e71cf25a1e885c37ee72372bf9abd0de168
Charles Day <charlesday99@outlook.com>
2020-06-27 15:38:33 +0100

    Inital project files including a simple web page and the necessary files for it.

---
+++ app/app.py
@@ -0,0 +1,38 @@
+from flask import redirect, url_for, render_template, abort, send_from_directory, send_file, jsonify, request
+from flask import Flask
+
+from gevent.pywsgi import WSGIServer
+import json
+import os
+
+
+#Create app
+app = Flask(__name__,static_url_path='')
+app.debug = False
+
+
+#Global variables
+ICON_LIST = {}
+ICON_PATH = os.path.join('static', 'icons')
+ROTATE_VALUES = {0,90,180,270}
+CURRENT_ROTATION = 0
+LOW_LIGHT = False
+
+
+#Landing page
+@app.route('/')
+def landing():    
+    return render_template('home.html')
+
+
+#Serve favicon for all calls
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+#Start point
+if __name__ == "__main__":
+    http_server = WSGIServer(('0.0.0.0', 80), app)
+    print("Loaded as HTTP Server on port 80, running forever:")
+    http_server.serve_forever()
