bb614a55899e5d6268b905e1d52e2688cd78576e
Charles Day <charlesday99@outlook.com>
2020-06-28 15:32:26 +0100

    Added error handling, added placeholder error pages and added a check to see if a post exists before rendering it.

--- app/app.py
+++ app/app.py
@@ -25,9 +25,11 @@ def landing():
 #Landing page
 @app.route('/event/<string:name>')
 def event_page(name):
-    content = open(os.path.join(EVENTS_PATH,(name + ".html"))).read()
-    print(content)
-    return render_template('event.html', content=content, title="Test")
+    if name in EVENTS_LIST:
+        content = open(os.path.join(EVENTS_PATH,(name + ".html"))).read()
+        return render_template('event.html', content=content, title="Test")
+    else:
+        return abort(404)
 
 
 #Serve favicon for all calls
@@ -36,6 +38,30 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
+#Handle 400 Error
+@app.errorhandler(400)
+def error_400(e):
+    return render_template('error.html',
+                           code=400,
+                           about="Invalid request, try again with different values!"),400
+#Handle 404 Error
+@app.errorhandler(404)
+def error_404(e):
+    return render_template('error.html',
+                           code=404,
+                           about="The file/service was not found, try again!"),404
+#Handle 418 Error
+@app.errorhandler(418)
+def error_418(e):
+    return render_template('error.html',
+                           code=418,
+                           about="I'm a teapot. This server is a teapot, not a coffee machine."),418
+#Handle 500 Error
+@app.errorhandler(500)
+def error_500(e):
+    return render_template('error.html',
+                           code=500,
+                           about="Something on the server went very wrong, Sorry!"),500
 #Start point
 if __name__ == "__main__":
     http_server = WSGIServer(('0.0.0.0', 80), app)
